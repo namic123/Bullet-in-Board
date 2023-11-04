08bf7fdf9f6822bf7bde85aeed1eaf452686c7a4
jay sung <namic123@naver.com>
2023-11-04 22:51:55 +0900

    프로시저 테스트 데이터 생성

--- board/sql/board.sql
+++ board/sql/board.sql
@@ -5,3 +5,17 @@ CREATE TABLE board (
     title VARCHAR(45) NOT NULL,
     content TEXT NOT NULL
 )
+
+
+# MySQL 프로시저 testDataInsert는 테스트 데이터를 board 테이블에 삽입하기 위한 것
+# 프로시저를 실행하면, '제목1', '내용1'부터 시작하여 '제목120', '내용120'까지 총 120개의 행이 board 테이블에 삽입
+CREATE PROCEDURE testDataInsert()
+BEGIN
+    DECLARE i INT DEFAULT 1;
+
+    WHILE i <= 120 DO
+        INSERT INTO board(title, content)
+            VALUES (concat('제목',i), concat('내용',i));
+        SET i = i +1;
+        end while;
+end;
