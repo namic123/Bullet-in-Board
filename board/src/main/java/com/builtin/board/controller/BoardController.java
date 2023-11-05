ca37a95837b3224c3cc0f4d94bc24069345ba566
jay sung <namic123@naver.com>
2023-11-06 00:03:14 +0900

    파일 업로드 구현 (저장 경로 및 uuid+ 파일이름)

--- board/src/main/java/com/builtin/board/controller/BoardController.java
+++ board/src/main/java/com/builtin/board/controller/BoardController.java
@@ -6,7 +6,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,8 +21,9 @@ public class BoardController {
     }
 
     @PostMapping("/board/writepro")
-    public String boardWritePro(Board board, Model model){
-        boardService.write(board);
+    public String boardWritePro(Board board, Model model, MultipartFile file)throws Exception{
+
+        boardService.write(board, file);
 
         model.addAttribute("message","글 작성이 완료되었습니다.");
         model.addAttribute("searchUrl","/board/list");
@@ -51,11 +55,12 @@ public class BoardController {
     }
     @PostMapping("/board/update/{id}")
     public String boardUpdate(@PathVariable("id") Integer id, Board board,
-                              RedirectAttributes rttr){
+                              MultipartFile file,
+                              RedirectAttributes rttr) throws Exception {
         Board boardTemp = boardService.boardView(id); // db에 해당 id 값을 가진 레코드를 가져옴
         boardTemp.setTitle(board.getTitle());   // 수정된 title을 set
         boardTemp.setContent(board.getContent());  // 수정된 content를 set
-        boardService.write(boardTemp);  // db에 변경사항 write
+        boardService.write(boardTemp, file);  // db에 변경사항 write
 
         rttr.addFlashAttribute("message",id +"번의 게시글이 수정되었습니다.");
         return "redirect:/board/list";
--- board/src/main/java/com/builtin/board/service/BoardService.java
+++ board/src/main/java/com/builtin/board/service/BoardService.java
@@ -4,15 +4,37 @@ import com.builtin.board.entity.Board;
 import com.builtin.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class BoardService {
     private final BoardRepository boardRepository;
     // 글 작성 처리
-    public void write(Board board){
+    public void write(Board board, MultipartFile file) throws IOException {
+       // Board 엔티티의 파일 저장 로직
+
+        // System.getProperty() - 시스템 속성 값을 검색
+        // user.dir 속성 - 애플리케이션이 실행되고 있는 디렉토리의 절대 경로
+        //"\\src\\main\\resources\\static\\files"; - 파일이 저장될 경로
+        String projectPath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\files";
+
+        // UUID (Universally Unique Identifier) - 고유한 이름을 생성
+        UUID uuid = UUID.randomUUID();
+        // uuid(고유한 값) + _ +기본 파일이름을 저장
+        String fileName = uuid +"_"+file.getOriginalFilename();
+
+        // 파일 경로와 이름을 지정
+        File saveFile = new File(projectPath,fileName);
+        // MultipartFile.transferTo - 업르드된 파일을 지정된 위치에 저장
+        file.transferTo(saveFile);
+
+        // board 엔티티를 DB에 저장
         boardRepository.save(board);
     }
 
